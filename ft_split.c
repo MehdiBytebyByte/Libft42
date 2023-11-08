d85cf36442b17b9bdfccb2c81da2045246e08c09
allthegoodusernamesarealreadytaken <holoyoda@gmail.com>
2023-11-08 15:27:50 +0100

    t

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,19 @@
 /*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:30:25 by mboughra          #+#    #+#             */
-/*   Updated: 2023/11/08 13:54:13 by mboughra         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:26:14 by mboughra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+
+// char *ft_freeme(char **str, int i)
+// {
+// 	if(str[i])	
+// }
+
+
 
 int ft_subscounter(char *str, char c)
 {
@@ -31,11 +39,22 @@ int ft_subscounter(char *str, char c)
 		return(1);
 	return count;
 }
-char *allocopy(*str,c,len)
+char *allocopy(char *str,char c,int len)
 {
-	str = (char *)malloc(len);
-	if(!str)
+	char *copy;
+	int i;
+	i = 0;
+	copy = (char *)malloc(sizeof(char)*len);
+	if(!copy)
 		return(NULL);
+	while(i < len -1)
+	{
+		copy[i] = str[i];
+		i++;
+	}
+
+	copy[i] = '\0';
+	return(copy);
 }
 
 #include <stdio.h>
@@ -50,20 +69,39 @@ char	**ft_split(char const *s, char c)
 	if (!s || c == '\0')
 		return(NULL);
 	subs = ft_subscounter((char *)s,c);
-	ret = (char **)malloc(subs + 1);
+	ret = malloc(sizeof(char *) * (subs + 1));
 	if(!ret)
 		return(NULL);
 	start = 0;
-	while(s[end])
+	end = 0;
+	i = 0;
+	while(i <= subs)
 	{
-		while(s[end] != c)
+		while(s[end] && s[end] != c)
 			end++;
-	
-	}
+		ret[i] = allocopy(((char *)s + start),c,(end - start) +1);
+		if(!ret[i])
+		{
+			//adding a function that frees here later
+			return(NULL);
+		}
+		start = end + 1;
+		end = start;
+		i++;
+		}
+		ret[i] = NULL;
+		return(ret);
 }
-
 int main()
 {
-	printf("%d",ft_subscounter("welcome to the summoners rift",'m'));
+    char s[] = "ap,pl,,,, ad ,aa,,ds,d,sd,sd, d,s d,s,e";
+    char c = ',';
+
+    char **result = ft_split(s, c);
+
+    for (int i = 0; result[i] != NULL; i++) 
+    {
+        printf("%d: %s\n", i, result[i]);
+    }
 }
 	// *(ret+j) == ret[j]
