dbc9b720b0b8cf360cf7ce4da47d1c3914897f82
allthegoodusernamesarealreadytaken <holoyoda@gmail.com>
2023-11-08 14:01:46 +0100

    finished

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:30:25 by mboughra          #+#    #+#             */
-/*   Updated: 2023/11/07 00:20:08 by mboughra         ###   ########.fr       */
+/*   Updated: 2023/11/08 13:54:13 by mboughra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,56 @@
 
 int ft_subscounter(char *str, char c)
 {
+	int count;
 	int i;
-	int j;
-	
-	j = 0;
+
 	i = 0;
+	count = 0;
 	while (str[i])
 	{
-		if(str[i] == c && str[i+1] != c && i != 0 && str[i+1] != '\0')
-			j++;
-	i++;
+		if(str[i] != c && i != 0 && str[i-1] == c )
+			count++;
+		i++;
 	}
-	return j;
+	if(str[0] != c && str[1] != c)
+		count++;
+	if(count == 0)
+		return(1);
+	return count;
 }
-
-
-// #include <stdio.h>
-// char	**ft_split(char const *s, char c)
-// {
-// 	if (!s || c == '\0')
-// 		return(NULL);
-	
-// }
-
-
-int	main(void)
+char *allocopy(*str,c,len)
 {
-	printf("%d",ft_subscounter("gleague of leggggends is a video gameggggg",'g'));		
+	str = (char *)malloc(len);
+	if(!str)
+		return(NULL);
 }
+
+#include <stdio.h>
+char	**ft_split(char const *s, char c)
+{
+	char **ret;
+	int	subs;
+	int start;
+	int end;
+	int i;
+	
+	if (!s || c == '\0')
+		return(NULL);
+	subs = ft_subscounter((char *)s,c);
+	ret = (char **)malloc(subs + 1);
+	if(!ret)
+		return(NULL);
+	start = 0;
+	while(s[end])
+	{
+		while(s[end] != c)
+			end++;
+	
+	}
+}
+
+int main()
+{
+	printf("%d",ft_subscounter("welcome to the summoners rift",'m'));
+}
+	// *(ret+j) == ret[j]
